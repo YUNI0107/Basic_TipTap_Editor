db854b7a26de2a253ebc5ca72df2e41cd19530c6
Yuni.Chou <yuni.chou@cloud-interactive.com>
2022-02-08 17:45:26 +0800

    Practice command & extention

--- src/MyTiptap.js
+++ src/MyTiptap.js
@@ -3,6 +3,9 @@ import { useState, useEffect } from 'react'
 import 'remixicon/fonts/remixicon.css'
 import { useEditor, EditorContent } from '@tiptap/react'
 import StarterKit from '@tiptap/starter-kit'
+import Paragraph from '@tiptap/extension-paragraph'
+import Heading from '@tiptap/extension-heading'
+import Link from '@tiptap/extension-link'
 
 import './styles.scss'
 
@@ -11,9 +14,37 @@ import MyMenu from './components/MyMenu/MyMenu'
 function MyTiptap() {
   const [editable, setEditable] = useState(false)
 
+  const CustomParagraph = Paragraph.extend({
+    addCommands() {
+      return {
+        paragraph:
+          () =>
+          ({ commands }) => {
+            return commands.setNode('paragraph')
+          },
+        bold:
+          () =>
+          ({ commands }) => {
+            return commands.toggleMark('bold')
+          },
+      }
+    },
+  })
+
   const editor = useEditor({
     editable,
-    extensions: [StarterKit],
+    extensions: [
+      StarterKit,
+      CustomParagraph,
+      Heading.configure({
+        HTMLAttributes: {
+          class: 'my-custom-class',
+        },
+      }),
+      Link.configure({
+        autolink: true,
+      }),
+    ],
     content: `
       <p>Hello World!</p>
       <h1>Heading</h1>
--- src/components/MyMenu/MyMenu.jsx
+++ src/components/MyMenu/MyMenu.jsx
@@ -12,6 +12,30 @@ function MyMenu({ editor }) {
       >
         <i className="ri-bold"></i> Bold
       </button>
+      <button
+        onClick={() => editor.chain().focus().bold().run()}
+        style={{ background: editor.isActive('bold') ? '#FF5722' : '#4444' }}
+      >
+        Paragraph-Bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().paragraph().run()}
+        style={{
+          background: editor.isActive('paragraph') ? '#FF5722' : '#4444',
+        }}
+      >
+        Paragraph
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        style={{
+          background: editor.isActive('heading', { level: 1 })
+            ? '#FF5722'
+            : '#4444',
+        }}
+      >
+        H1
+      </button>
     </div>
   )
 }
